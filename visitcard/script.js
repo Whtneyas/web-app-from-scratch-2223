ceb09ca1443feac4cbbe1d3542fc9b79a954779f
Whitneyas <whitneyserwaa@gmail.com>
2023-02-12 22:02:21 +0100

    name added

--- visitcard/script.js
+++ visitcard/script.js
@@ -1,19 +1,23 @@
-const h1 = document.querySelector("h1")
+// Variablesconst 
 
-const squadUrl =  "https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/squad?id=cldcspecf0z0o0bw59l8bwqim";
+myMemberUrl = "https://whois.fdnd.nl/api/v1/member?id=cldeoyw463vaj0bw594n8mhn4";
+// HTML elements
+let naam = document.querySelector(".naam");
+let surname = document.querySelector(".surname");
 
-// give temporarily access on https://cors-anywhere.herokuapp.com/corsdemoconst 
 
 
+const insertName= async (API) => {
 
+     const myData = await fetch(API)
+        .then((response) => response.json())
+        .then((myData) => myData.member);
 
-const myData = fetch(squadUrl)  
-              .then((response) => response.json())  
-              .then((myData) =>
+         console.log(myData);
 
-               console.log(myData.squad.members[7]));
-
-            
-
+       naam.innerHTML = myData.name;
+       surname.innerHTML = myData.surname;
+    };
+     insertName(myMemberUrl);
 
 
